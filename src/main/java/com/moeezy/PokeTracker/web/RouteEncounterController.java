78c40231689aeed27ee1acd93f433dac581f944e
moeezy <moeezshahid6341@gmail.com>
2026-06-28 18:03:45 -0400

    API for Mapped Encounters

--- src/main/java/com/moeezy/PokeTracker/web/RouteEncounterController.java
+++ src/main/java/com/moeezy/PokeTracker/web/RouteEncounterController.java
@@ -5,14 +5,12 @@ import com.moeezy.PokeTracker.data.entity.RouteEncounter;
 import com.moeezy.PokeTracker.service.RouteEncounterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/v1/RouteEncounter")
 public class RouteEncounterController {
     private final RouteEncounterService routeEncounterService;
